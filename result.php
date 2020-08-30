3e179784920314204368da024c58d1d00a31b753
Megumi Kutsuzawa <mgm.ktzw07@gmail.com>
2020-08-30 02:38:02 +0000

    Laravel_13追加課題

---
+++ result.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charsert="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=divice-width, initial-scale=1">
+        
+        <title>テストフォーム</title>
+    </head>
+    
+    <body>
+        <div>
+            <h1>このページは、フォームで送信された食べ物を受け取るページです</h1>
+        </div>
+    </body>
+</html>
